78c79cc8ff8e373b45fb02bbeecfa53e81622957
komanushi <155377273+komanushi@users.noreply.github.com>
2024-03-05 01:18:36 +0900

    [add] rq の自動リロード

--- application/modules/analyzer/converter.py
+++ application/modules/analyzer/converter.py
@@ -6,6 +6,6 @@ def convert_to_candfans_user_model(user: CandfansUser) -> CandfansUserModel:
     return CandfansUserModel(
         user_id=user.user_id,
         user_code=user.user_code,
-        user_name=user.user_name,
+        username=user.username,
         last_synced_at=user.last_synced_at,
     )
