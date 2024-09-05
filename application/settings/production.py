454ca3c39b8abe0d4b66606b9544abb4d31cccd3
komanushi <155377273+komanushi@users.noreply.github.com>
2024-09-06 00:00:18 +0900

    [add] sentry

--- application/settings/production.py
+++ application/settings/production.py
@@ -1,4 +1,6 @@
 import os
+import sentry_sdk
+
 from .base import *
 
 DEBUG = False
@@ -9,3 +11,9 @@ SERVICE_HOST = f"https://candfans-analyzer.onrender.com/"
 CSRF_TRUSTED_ORIGINS = [
     f"https://{os.environ['SERVICE_DOMAIN']}",
 ]
+
+sentry_sdk.init(
+    dsn="https://34d8a62fba5b0f9900fe8d50ad581ea9@o4507900831399936.ingest.us.sentry.io/4507900832841728",
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
